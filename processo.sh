1b8c0f0e39190d97fe2f49a413ffd8377dc6af65
PH <46381770+phscuritiba@users.noreply.github.com>
2019-12-02 19:55:48 -0300

    Update processo.sh

--- processo.sh
+++ processo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 	
 #Faz o download dos arquivos .zip do site da Recieta Federal
-	for arquivo in {01..02}; do wget -c -A zip "http://200.152.38.155/CNPJ/DADOS_ABERTOS_CNPJ_"$arquivo".zip" -P /var/www/arqsreceita; done
+	for arquivo in {01..20}; do wget -c -A zip "http://200.152.38.155/CNPJ/DADOS_ABERTOS_CNPJ_"$arquivo".zip" -P /var/www/arqsreceita; done
 	
 #Executa o .py para converter para .csv
 	python3 /var/www/cnpj.py /var/www/ csv /var/www/csv --dir
